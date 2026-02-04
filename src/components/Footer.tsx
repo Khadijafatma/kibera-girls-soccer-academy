698d91fe4cc3c242c5986b96b3cd9070a8701c68
Khadija fatma <khadijafatma404@gmail.com>
2026-02-04 15:54:21 +0300

    fixing bugs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -68,7 +68,7 @@ const Footer = () => {
 
         <div className="border-t border-primary-foreground/20 mt-8 pt-8 text-center">
           <p className="text-primary-foreground/60">
-            © 2025
+            © 2026
              KGSA. All rights reserved. Built with care for education.
           </p>
         </div>
